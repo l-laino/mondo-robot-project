b303681f7d1327e3e1330dd3f3654d34533c0d92
berga02 <fabio.berga02@gmail.com>
2023-05-04 11:13:31 +0200

    temporaneo

--- MondoRobot/src/menu/ControllerMenu.java
+++ MondoRobot/src/menu/ControllerMenu.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionListener;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
-import gameHandler.ControllerGame;
+import game.ControllerGame;
 
 public class ControllerMenu implements ActionListener{
 	private ModelMenu m;
@@ -41,6 +41,7 @@ public class ControllerMenu implements ActionListener{
 			try{
 				m.setDim(v.getTextField());
 				if(m.getDim() < 10) new Exception();
+				new ControllerGame();
 				// Avvio il thread del conteggio dei secondi
 				new ControllerGame();						 
 			}catch(Exception invalidInput){
